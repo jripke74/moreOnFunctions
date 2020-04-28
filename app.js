b0396828234815eca1e898f4d3e5a666a58c6ff2
jripke74 <jripke74@gmail.com>
2020-04-28 11:53:10 -0700

    add anonymous function

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const startGameBtn = document.getElementById('start-game-btn');
 
-function startGame() {
-  console.log('Game is starting...');
-}
 
-startGame();
+startGameBtn.addEventListener('click', function() {
+  console.log('Game is starting...');
+});
